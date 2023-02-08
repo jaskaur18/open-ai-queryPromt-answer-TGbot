3ee554c6c63ba98160e4c54dbbe5b733df4963e0
jaskaur18 <jaskaurhello@gmail.com>
2023-02-08 23:39:55 +0530

    removed mongodb

--- src/helpers/startMongo.ts
+++
@@ -1,9 +0,0 @@
-import { connect, set } from 'mongoose'
-import env from '@/helpers/env'
-
-function startMongo() {
-  set('strictQuery', true)
-  return connect(env.MONGO)
-}
-
-export default startMongo
