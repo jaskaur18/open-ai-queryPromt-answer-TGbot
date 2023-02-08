46c0a5451a81dc7990924c128ccce82fc1fda603
jaskaur18 <jaskaurhello@gmail.com>
2023-02-08 23:44:36 +0530

    removed mongodb

--- src/handlers/commands/start.ts
+++ src/handlers/commands/start.ts
@@ -1,15 +1,7 @@
-import { findOrCreateUser } from '@/models/User'
 import Context from '@/models/Context'
 import sendOptions from '@/helpers/sendOptions'
 
 export default async function handleStart(ctx: Context) {
   if (!ctx.from) return
-
-  await findOrCreateUser(
-    ctx.from.id,
-    ctx.from.username,
-    ctx.from.first_name,
-    ctx.from.last_name
-  )
   return ctx.reply(ctx.t('welcome'), sendOptions(ctx))
 }
